95ecc0fc2e9c833e655b117b2b84f8edf17610c2
Bill Johnson <wpj174@gmail.com>
2021-11-06 15:19:14 -0400

    Query 4
    Working on it.

--- query.sql
+++ query.sql
@@ -1,7 +1,7 @@
 -- Queries
 
 -- 1. List the following details of each employee: employee number, last name, first name, sex,
--- and salary.
+--    and salary.
 select
 	id as "Employee Number",
 	last_name as "Last Name",
@@ -22,7 +22,7 @@ where date_part('year', hire_date) = 1986;
 
 
 -- 3. List the manager of each department with the following information: department number,
--- department name, the manager's employee number, last name, first name.
+--    department name, the manager's employee number, last name, first name.
 select
 	dept_id as "Department Number",
 	name as "Department Name",
@@ -34,13 +34,28 @@ join employee on department_manager.emp_id = employee.id
 join department on department_manager.dept_id = department.id;
 
 
--- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+-- 4. List the department of each employee with the following information: employee number,
+--    last name, first name, and department name.
+select
+	employee.id as "Employee Number",
+	last_name as "Last Name",
+	first_name as "First Name",
+	dept_id
+from employee
+left join department_employee on department_employee.emp_id = employee.id;
+--join department on department.id = department_employee.dept_id;
 
--- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select * from department_employee;
 
--- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules"
+--    and last names begin with "B."
 
--- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- 6. List all employees in the Sales department, including their employee number, last name,
+--    first name, and department name.
 
--- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+-- 7. List all employees in the Sales and Development departments, including their employee number,
+--    last name, first name, and department name.
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many
+--    employees share each last name.
 

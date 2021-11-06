714db22be57a73803cd31c3990dcb765ae3f690f
Bill Johnson <wpj174@gmail.com>
2021-11-06 14:56:17 -0400

    Query 2

--- query.sql
+++ query.sql
@@ -13,6 +13,10 @@ join salary on employee.id = salary.emp_id;
 
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
+select first_name, last_name, hire_date
+from employee
+where date_part('year', hire_date) = 1986;
+
 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
